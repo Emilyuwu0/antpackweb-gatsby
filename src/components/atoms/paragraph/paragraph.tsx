c80d74ba36390ac3721f77760fc7aa2215d04b3d
yeisson stik gutierrez <stik@yeissons-MacBook-Air.local>
2022-10-27 08:33:42 -0500

    responsive settings

--- src/components/atoms/paragraph/paragraph.tsx
+++ src/components/atoms/paragraph/paragraph.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './paragraph.scss'
+const HtmlToReactParser = require('html-to-react').Parser;
 
 interface titleProps {
   text: string
 }
 
 export default function Paragraph({ text }: titleProps) {
+  const htmlParce = new HtmlToReactParser();
   return (
-    <p className='paragraph'>{text}</p>
+    <p className='paragraph'>{htmlParce.parse(text)}</p>
   )
 }
