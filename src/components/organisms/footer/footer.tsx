38489c92050ac0a25ccfb7b1f4ceac7c50df2726
yeisson stik gutierrez <stik@yeissons-MacBook-Air.local>
2023-01-23 13:29:32 -0500

    footer settings

--- src/components/organisms/footer/footer.tsx
+++ src/components/organisms/footer/footer.tsx
@@ -14,80 +14,78 @@ import './footer.scss'
 export default function Footer() {
   return (
     <div className='footerContainer'>
-      <div className='seccion1'>
-        <ul>
-          <li>
-            <OutboundLink href='#ourCraft'>
-              <Title level='h3' text='Our craft' />
-            </OutboundLink>
-          </li>
-          <li>
-            <OutboundLink href='#services'>
-              <Title level='h3' text='Services' />
-            </OutboundLink>
-          </li>
-          <li>
-            <OutboundLink href='#company'>
-              <Title level='h3' text='Company' />
-            </OutboundLink>
-          </li>
-          <li>
-            <OutboundLink href='#contact'>
-              <Title level='h3' text='Contact' />
-            </OutboundLink>
-          </li>
-        </ul>
-        <div className='logo'>
-          <LogoAntpackBlack />
+      <div className='footerWrapper'>
+        <div className='seccion1'>
+          <ul>
+            <li>
+              <OutboundLink href='#ourCraft'>
+                <Title level='h3' text='Our craft' />
+              </OutboundLink>
+            </li>
+            <li>
+              <OutboundLink href='#services'>
+                <Title level='h3' text='Services' />
+              </OutboundLink>
+            </li>
+            <li>
+              <OutboundLink href='#company'>
+                <Title level='h3' text='Company' />
+              </OutboundLink>
+            </li>
+            <li>
+              <OutboundLink href='#contact'>
+                <Title level='h3' text='Contact' />
+              </OutboundLink>
+            </li>
+          </ul>
+          <div className='logo'>
+            <LogoAntpackBlack />
+          </div>
+        </div>
+        <div className='seccion2'>
+          <div className='infoSec'>
+            <div className='InfoText'>
+              <Title level='h4' text='BOGOTÁ' />
+              <p> <OutboundLink href="tel:+57(601)4813202">+57 (601)4813202</OutboundLink>
+                <br />
+                Calle 100 #13-21 OF. 701</p>
+            </div>
+
+            <div className='InfoText'>
+              <Title level='h4' text='ANTPACK' />
+              <p>
+                <OutboundLink href='mailto:hello@antpack.co'>hello@antpack.co</OutboundLink>
+              </p>
+            </div>
+          </div>
+
+          <div className='linksContainer'>
+            <div>
+              <OutboundLink target={'_blank'} href='https://co.linkedin.com/company/antpack'>
+                <Linkedin />
+              </OutboundLink>
+
+              <OutboundLink target={'_blank'} href='https://www.instagram.com/ant.pack/'>
+                <Intagram />
+              </OutboundLink>
+
+              <OutboundLink target={'_blank'} href='https://www.behance.net/antpack'>
+                <Be />
+              </OutboundLink>
+
+              <OutboundLink target={'_blank'} href='https://api.whatsapp.com/send?phone=573214444727&text=Hello'>
+                <Whatsapp />
+              </OutboundLink>
+            </div>
+
+            <div className='terms'>
+              <Link to=''>Terms & Privacy</Link>
+            </div>
+
+          </div>
         </div>
       </div>
-      <div className='seccion2'>
-        <div className='infoSec'>
-          <div className='InfoText'>
-            <Title level='h4' text='BOGOTÁ' />
-            <p> <OutboundLink href="tel:+57(601)4813202">+57 (601)4813202</OutboundLink>
-              <br />
-              Calle 100 #13-21 OF. 701</p>
-          </div>
-
-          <div className='InfoText'>
-            <Title level='h4' text='ANTPACK' />
-            <p>
-              <OutboundLink href='mailto:hello@antpack.co'>hello@antpack.co</OutboundLink>
-            </p>
-          </div>
-        </div>
-
-        <hr />
-
-        <div className='linksContainer'>
-          <div>
-            <OutboundLink target={'_blank'} href='https://co.linkedin.com/company/antpack'>
-              <Linkedin />
-            </OutboundLink>
-
-            <OutboundLink target={'_blank'} href='https://www.instagram.com/ant.pack/'>
-              <Intagram />
-            </OutboundLink>
-
-            <OutboundLink target={'_blank'} href='https://www.behance.net/antpack'>
-              <Be />
-            </OutboundLink>
-
-            <OutboundLink target={'_blank'} href='https://api.whatsapp.com/send?phone=573214444727&text=Hello'>
-              <Whatsapp />
-            </OutboundLink>
-          </div>
-
-          {/* <div className='terms'>
-            <Link to='https://www.facebook.com'>Terms & Privacy</Link>
-          </div> */}
-
-        </div>
-        <div className='logo'>
-          <LogoAntpackBlack />
-        </div>
-      </div>
+      <hr />
     </div>
   )
 }
--- src/components/organisms/partners/partners.tsx
+++ src/components/organisms/partners/partners.tsx
@@ -1,15 +1,101 @@
 import './partners.scss'
 
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+import Marquee from 'react-fast-marquee'
 
 export default function Partners() {
   return (
-    <div>
-      <div>
-        <h3>partners</h3>
-      </div>
+    <div className='partnersContainer'>
+      <div className='partnersWrapper'>
+        <div className='titleContainer'>
+          <h3>partners</h3>
+        </div>
 
-      <div>
+        <div className='logosContainer'>
+          <Marquee
+            direction={'right'}
+            speed={10}
+            loop={0}
+            style={{ height: '100px', width: '100%' }}
+            gradient={false}
+            className='partnersMarque'
+          >
+            <div className='partnerLogo'>
+              <a href="">
+                <StaticImage
+                  src='../../../assets/images/partnersImages/AccreditedAgency1.png'
+                  alt=''
+                  quality={100}
+                />
+              </a>
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/awsCertified.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/googleDev.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/mercadoPago.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/scrumAliance.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/shopifyPlusPartner.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/theEyePartner.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/vmware.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+
+            <div className='partnerLogo'>
+              <StaticImage
+                src='../../../assets/images/partnersImages/vtexPartner.png'
+                alt=''
+                quality={100}
+              />
+            </div>
+          </Marquee>
+        </div>
 
       </div>
 
